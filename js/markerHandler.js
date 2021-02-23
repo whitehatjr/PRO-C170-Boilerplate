27f921092d4f4805d0e2c2cf8aeaaf3c722582ad
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-02-23 14:47:14 +0530

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,62 @@
+AFRAME.registerComponent("markerhandler", {
+  init: async function() {
+    
+    var dishes = await this.getDishes();
+    
+    this.el.addEventListener("markerFound", () => {
+      var markerId = this.el.id;
+      this.handleMarkerFound(dishes, markerId);
+    });
+
+    this.el.addEventListener("markerLost", () => {
+      this.handleMarkerLost();
+    });
+  },
+  handleMarkerFound: function(dishes, markerId) {
+    // Changing button div visibility
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "flex";
+
+    var ratingButton = document.getElementById("rating-button");
+    var orderButtton = document.getElementById("order-button");    
+
+    // Handling Click Events
+    ratingButton.addEventListener("click", function() {
+      swal({
+        icon: "warning",
+        title: "Rate Dish",
+        text: "Work In Progress"
+      });
+    });
+
+    orderButtton.addEventListener("click", () => {
+      swal({
+        icon: "https://i.imgur.com/4NZ6uLY.jpg",
+        title: "Thanks For Order !",
+        text: "Your order will serve soon on your table!"
+      });
+    });
+
+    // Changing Model scale to initial scale
+    var dish = dishes.filter(dish => dish.id === markerId)[0];
+    var model = document.querySelector(`#model-${dish.id}`);
+    model.setAttribute("position", dish.model_geometry.position);
+    model.setAttribute("rotation", dish.model_geometry.rotation);
+    model.setAttribute("scale", dish.model_geometry.scale);
+  },
+  
+  handleMarkerLost: function() {
+    // Changing button div visibility
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "none";
+  },
+  getDishes: async function() {
+    return await firebase
+      .firestore()
+      .collection("dishes")
+      .get()
+      .then(snap => {
+        return snap.docs.map(doc => doc.data());
+      });
+  }
+});
